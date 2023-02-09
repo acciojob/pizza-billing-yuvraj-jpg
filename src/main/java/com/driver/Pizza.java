6edfeebe586d67744b2cf6eaac743ce08824ba73
Yuvraj <yv96875@gmail.com>
2023-02-09 19:56:17 +0530

    Pizza billing

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,33 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-
+    private int cheeseprice;
+    private int topping;
+    private int takeaway;
+    private Boolean addExtraCheese;
+    private Boolean addExtraToppings;
+    private Boolean addTakeaway;
+    private Boolean calculateBill;
+    
+    
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        this.cheeseprice=80;
+        this.takeaway=20;
+        if(isVeg==true){
+            this.price = 300;
+            this.topping = 70;
+        }
+        else{
+            this.price = 400;
+            this.topping = 120;
+        }
+        this.addExtraCheese=false;
+        this.addExtraToppings=false;
+        this.addTakeaway=false;
+        this.calculateBill=false;
+
+        this.bill = "Base Price Of The Pizza:" + this.price + "\n";
     }
 
     public int getPrice(){
@@ -16,19 +39,47 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(this.addExtraCheese==false){
+            price = price + this.cheeseprice;
+            this.addExtraCheese=true;
+        }
+        
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        
+        if(this.addExtraToppings==false){
+            price = price + this.topping;
+            this.addExtraToppings=true;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(this.addTakeaway==false){
+            price = price + this.takeaway;
+            this.addTakeaway=true;
+        }
     }
 
     public String getBill(){
-        // your code goes here
+        if(this.calculateBill==false){
+            // check for cheese
+            if(this.addExtraCheese==true){
+                this.bill = this.bill + "Extra Cheese Added: " + this.cheeseprice + "\n"; 
+            }
+            // check for tooping
+            if(this.addExtraToppings==true){
+                this.bill = this.bill +"Extra Toppings Added: "+ this.topping + "\n";
+            }
+            // check for takeaway
+            if(this.addTakeaway==true){
+                this.bill = this.bill +"Paperbag Added: "+ this.takeaway + "\n";
+            }
+            this.bill = this.bill + this.price;
+            calculateBill = true;
+
+        }
+
         return this.bill;
     }
 }
